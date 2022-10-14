07407a85aea0e91b5202601f8040be29e9165ad9
David Rajcher <davidrajcher@Davids-MacBook-Pro-2.local>
2022-10-14 14:00:57 +0200

    lecture content

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const main = document.getElementById("main");
+const API_BASE = "https://dummyjson.com";
 
-const createProductCarts = (products) => {
+const view = () => {
+  const url = window.location.href.split("/");
+  const file = url[url.length - 1];
+  const fileName = file.split(".")[0];
+  if (!fileName) {
+    return "index";
+  }
+  return fileName;
+};
+
+const createProductCards = (products) => {
+  const main = document.getElementById("main");
+
   products.forEach((product) => {
-    console.log(product);
-
     const { id, title, description, price, rating, thumbnail } = product;
     const card = document.createElement("div");
     card.classList.add("card");
@@ -46,12 +56,164 @@ const createProductCarts = (products) => {
   });
 };
 
-const fetchProducts = async () => {
-  const response = await fetch("https://dummyjson.com/products");
-  const { products } = await response.json();
+const renderCart = (cart, products) => {
+  console.log("render cart", cart);
+  const main = document.getElementById("main");
+  main.innerHTML = "";
 
-  // console.log(products);
-  createProductCarts(products);
+  const cartContainer = document.createElement("div");
+  cartContainer.classList.add("cart-container");
+
+  cart.forEach((cartItem) => {
+    const item = products.find(
+      (product) => product.id === parseInt(cartItem.id)
+    );
+
+    const itemContainer = document.createElement("div");
+    itemContainer.classList.add("item-container");
+
+    const titleEl = document.createElement("p");
+    const titleText = document.createTextNode(item.title);
+    titleEl.appendChild(titleText);
+
+    const amountEl = document.createElement("p");
+    const amonutText = document.createTextNode(cartItem.amount);
+    amountEl.appendChild(amonutText);
+
+    const increaseButton = document.createElement("button");
+    const increaseButtonText = document.createTextNode("+");
+    increaseButton.classList.add("increase-button");
+    increaseButton.setAttribute("id", item.id);
+    increaseButton.appendChild(increaseButtonText);
+
+    const decreaseButton = document.createElement("button");
+    const decreaseButtonText = document.createTextNode("-");
+    decreaseButton.classList.add("decrease-button");
+    decreaseButton.setAttribute("id", item.id);
+    decreaseButton.appendChild(decreaseButtonText);
+
+    main.appendChild(cartContainer);
+    cartContainer.appendChild(itemContainer);
+    itemContainer.append(titleEl, increaseButton, amountEl, decreaseButton);
+  });
 };
 
-fetchProducts();
+let cart = [];
+
+// to add a new item to cart
+
+const addItemToCart = (e) => {
+  const { id } = e.target;
+
+  // check if item already in cart
+  const item = cart.find((item) => item.id === id);
+
+  // if no item, push a new one
+  if (!item) {
+    cart.push({
+      id,
+      amount: 1,
+    });
+    return;
+  }
+
+  // if in cart, increase number of items
+  item.amount++;
+
+  console.log("cart", cart);
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
+
+const rerenderCart = (cart) => {
+  renderCart(cart, JSON.parse(localStorage.getItem("products")));
+  localStorage.setItem("cart", JSON.stringify(cart));
+  addEventListenerToCartButton();
+};
+
+// increase number of items
+const increaseItems = (e) => {
+  console.log("increase items");
+
+  const { id } = e.target;
+  const item = cart.find((item) => {
+    return item.id === id;
+  });
+  item.amount++;
+
+  rerenderCart(cart);
+};
+
+// decrease number of items
+const decreaseItems = (e) => {
+  console.log("decrease items");
+  const { id } = e.target;
+  let itemIndex;
+  const item = cart.find((item, index) => {
+    itemIndex = index;
+    return item.id === id;
+  });
+
+  item.amount--;
+
+  // falsy => "", 0, false, undefined, null
+  if (item.amount === 0) {
+    // cart = cart.filter((cartItem) => cartItem.id !== item.id);
+    cart.splice(itemIndex, 1);
+  }
+
+  rerenderCart(cart);
+};
+
+// delete items
+
+// show total
+
+// render only cart products
+
+// enter cart view
+
+// create an event listener enterCartView and connect with cart button in navbar
+
+// exit cart view
+
+const addEventListenerToCartButton = () => {
+  const increaseButtons = document.getElementsByClassName("increase-button");
+  [...increaseButtons].forEach((button) => {
+    button.addEventListener("click", increaseItems);
+  });
+
+  const decreaseButtons = document.getElementsByClassName("decrease-button");
+  [...decreaseButtons].forEach((button) => {
+    button.addEventListener("click", decreaseItems);
+  });
+};
+
+(async () => {
+  const response = await fetch(`${API_BASE}/products?limit=100`);
+  const { products } = await response.json();
+
+  localStorage.setItem("products", JSON.stringify(products));
+
+  // Init
+  const pageName = view();
+  if (pageName === "index") {
+    createProductCards(products);
+
+    // connect add to cart function to add-to-cart button
+    const addToCartButtonsCollection =
+      document.getElementsByClassName("add-to-cart-button");
+
+    [...addToCartButtonsCollection].forEach((button) => {
+      button.addEventListener("click", addItemToCart);
+    });
+  }
+
+  if (pageName === "cart") {
+    renderCart(JSON.parse(localStorage.getItem("cart")), products);
+
+    cart = JSON.parse(localStorage.getItem("cart"));
+
+    addEventListenerToCartButton();
+  }
+})();
